f2f4ea41e24502f17f88586e5b39b11c59b69f07
Alejandra Guzman <aleguzman.gza@gmail.com>
2019-12-09 15:13:05 -0600

    new update

--- js/random.js
+++ js/random.js
@@ -2,9 +2,12 @@ async function getRandomBeers(){
     let response = await fetch(`https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/beers/?key=${sandboxKey}`)
     let beers = await response.json()
     let beerData= beers.data
+    console.log(beerData)
     
     let randomBeers = beerData[Math.floor(Math.random() * beerData.length)];
     console.log(randomBeers.name)
+    console.log(randomBeers.style.shortName)
+    
 }
 
 getRandomBeers()
