c1e0993459f4a7ca45af91ea622d1e8ee5022eaf
Jaclyn Legacion <jglegacion@gmail.com>
2019-12-10 17:24:04 -0600

    init commit of firebase setup

--- js/util/fbacct.js
+++ js/util/fbacct.js
@@ -1,42 +1,13 @@
-const signupBtn = document.getElementById("signupBtn");
-const loginBtn = document.getElementById("loginBtn");
-const loginPw = document.getElementById("login-pw");
-const loginEmail = document.getElementById("login-email");
-let signupEmail = document.getElementById("signup-email");
-let signupPw = document.getElementById("signup-pw");
-let signupConf = document.getElementById("pw-conf");
-
-// regex to check if email
-const isEmail = email => {
-    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (email.match(regEx)) return true;
-    else return false;
+const firebaseConfig = {
+    apiKey: "AIzaSyC6_YAVkkrBKppE4FNotOecIMHGJdoUHkk",
+    authDomain: "brews-7701b.firebaseapp.com",
+    databaseURL: "https://brews-7701b.firebaseio.com",
+    projectId: "brews-7701b",
+    storageBucket: "brews-7701b.appspot.com",
+    messagingSenderId: "741818176617",
+    appId: "1:741818176617:web:e64c457d9b95b49b06fef8",
+    measurementId: "G-K2XY9MWYF9"
 };
-
-
-signupBtn.addEventListener("click", () => {
-    const newUser = {
-        email: signupEmail.value,
-        password: signupPw.value,
-        confirmPassword: signupConf.value
-    }
-
-    if (isEmail(newUser.email) === true && newUser.password === newUser.confirmPassword) {
-        console.log("VALID INFO!")
-    } else if (isEmail(newUser.email) === false) {
-        console.log("INVALID EMAIL")
-    } else if (newUser.password === "" || newUser.confirmPassword === "") {
-        console.log("CANNOT BE EMPTY")
-    }
-
-})
-
-
-
-    // if (confirmSignup(signupEmail, signupPw, signupConf) === true) {
-    //     console.log("INFO IS VALID")
-    // } else if (confirmSignup(signupEmail, signupPw, signupConf) === false) console.log("INFO IS INVALID")
-
-
-
-// console.log(isEmail(`test@email.com`))
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
