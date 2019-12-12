48393de023e5aa15bc5818a8ad050192ac565629
Jaclyn Legacion <jglegacion@gmail.com>
2019-12-12 16:36:52 -0600

    find a new way to randomly choose a beer

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ let endpoint;
 
 const cors = "https://cors-anywhere.herokuapp.com/";
 
-const sandboxKey = "969c47b896fcbe4c5eb88f0e259f8bb1";
+const sandboxKey = "fce22c1d03a43332fde9d3f5fd72101c"; //969c47b896fcbe4c5eb88f0e259f8bb1";
 
 // find random beer
 randomBtn.addEventListener("click", () => {
@@ -13,29 +13,30 @@ randomBtn.addEventListener("click", () => {
 
     const sandboxURLRandom = `${cors}https://sandbox-api.brewerydb.com/v2/${endpoint}/${parameter}/?key=${sandboxKey}`
 
-    async function randomBeer() {
+    async function randomBeerInfo() {
         let response = await fetch(sandboxURLRandom);
         let beerData = await response.json();
         let beer = beerData.data;
         let beerID = beer.id;
-
-        let sandboxURLBeerID = `${cors}https://sandbox-api.brewerydb.com/v2/${endpoint}/${beerID}/?key=${sandboxKey}`
-
         beerName.innerHTML = beer.name;
         abv.innerHTML = beer.abv;
         beerStyle.innerHTML = beer.style.shortName;
+        let sandboxURLBeerID = `${cors}https://sandbox-api.brewerydb.com/v2/${endpoint}/${beerID}/?key=${sandboxKey}`
 
         if (beer.isRetired != "Y") {
             isRetired.innerHTML = `Beer is still in production!`
         } else {
             isRetired.innerHTML = `Beer is no longer in production.`
         }
+        hideSearch();
+        showBeerResults();
+        console.log(beerID);
 
         async function findRandomBeerInfo() {
             let response = await fetch(sandboxURLBeerID);
             let beerData = await response.json();
             let beer = beerData.data
-            console.log(beer)
+            // console.log(beer)
             // console.log("FOOBAR")
             if (beer.hasOwnProperty("labels")) {
                 label.src = beer.labels.medium;
@@ -57,11 +58,16 @@ randomBtn.addEventListener("click", () => {
             }
         }
         findRandomBeerInfo()
+
     }
-    // console.log(beer)
-    showBeerResults()
-    hideSearch()
-    randomBeer()
+    randomBeerInfo();
+    // beerID
+
+    // async function beerIDInfo() {
+    //     let sandboxURLBeerID = `${cors}https://sandbox-api.brewerydb.com/v2/${endpoint}/${beerID}/?key=${sandboxKey}`
+    // }
+
+
 })
 
 
