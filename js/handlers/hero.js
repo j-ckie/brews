2d9cddd90fc29bbdcfc026824338132aef2530a9
Jaclyn Legacion <jglegacion@gmail.com>
2019-12-12 09:46:24 -0600

    init commit of production branch

--- js/handlers/hero.js
+++ js/handlers/hero.js
@@ -1,20 +1,20 @@
-// const showHeroImg = () => {
-//     function getRandomHero() {
-//         let heroImgs = [
-//             "../img/hero/hero1.jpg",
-//             "../img/hero/hero2.jpg",
-//             "../img/hero/hero3.jpg",
-//             "../img/hero/hero4.jpg"
-//         ];
+const showHeroImg = () => {
+    function getRandomHero() {
+        let heroImgs = [
+            "../img/hero/hero1.jpg",
+            "../img/hero/hero2.jpg",
+            "../img/hero/hero3.jpg",
+            "../img/hero/hero4.jpg"
+        ];
 
-//         let randomNo = Math.floor(Math.random() * heroImgs.length - 1 + 1);
-//         return heroImgs[randomNo];
-//     }
+        let randomNo = Math.floor(Math.random() * heroImgs.length - 1 + 1);
+        return heroImgs[randomNo];
+    }
 
-//     let hero = getRandomHero();
+    let hero = getRandomHero();
 
-//     let randomHero = document.getElementById("beer-landing");
+    let randomHero = document.getElementById("beer-landing");
 
-//     randomHero.src = hero;
-//     console.log("FOOBAR")
-// }
+    randomHero.src = hero;
+    console.log("FOOBAR")
+}
